8fa7acbf99add7c4b2559cc5643455cd4ab5715c
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-13 11:49:31 +0530

    updated

--- src/Components/View.jsx
+++ src/Components/View.jsx
@@ -1,54 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import VideoCard from './VideoCard'
-import { Col, Row } from 'react-bootstrap'
-import { getAllVideos } from '../services/allAPI'
-function View({uploadVideoServerResponce}) {
+import React, { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import { Col, Row } from "react-bootstrap";
+import { getAllVideos } from "../services/allAPI";
+function View({ uploadVideoServerResponce }) {
+  const [deleteVideoStatus, setDeleteVideoStatus] = useState(false);
 
-  const [deleteVideoStatus,setDeleteVideoStatus]=useState(false)
+  const [allVideos, setAllVideos] = useState([]);
+  const getAllUploadedVideos = async () => {
+    try {
+      const { data } = await getAllVideos();
+      setAllVideos(data);
+    } catch (error) {}
+  };
 
-  const [allVideos,setAllVideos]=useState([])
-const getAllUploadedVideos = async() => {
-  try {
-    const {data} = await getAllVideos()
-    setAllVideos(data)
-   
-    
-     
+  useEffect(() => {
+    getAllUploadedVideos();
+    setDeleteVideoStatus(false);
+  }, [uploadVideoServerResponce, deleteVideoStatus]);
 
-
-
-
-
-  } catch (error) {
-
-  }
-}  
-
-useEffect(()=> {
-  getAllUploadedVideos()
-  setDeleteVideoStatus(false)
-},[uploadVideoServerResponce,deleteVideoStatus])
-
-  
   return (
     <Row>
-      {
-        allVideos.length>0 ? 
-  allVideos.map(video=> (
-<Col sm={12} md={6} lg={4} xl={3}>
-   <VideoCard  displayData={video}  setDeleteVideoStatus={setDeleteVideoStatus}/>   
-      </Col>
-
-  ))
-
-:
-<div>video library is empty</div>
-
-      }
-      
-     
-      </Row>
-  )
+      {allVideos.length > 0 ? (
+        allVideos.map((video) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <VideoCard
+              displayData={video}
+              setDeleteVideoStatus={setDeleteVideoStatus}
+            />
+          </Col>
+        ))
+      ) : (
+        <div>video library is empty</div>
+      )}
+    </Row>
+  );
 }
 
-export default View
+export default View;
